c1c89ade29ff04bab117151cef43c20130eb1324
Su Yadanar <suyadanar@innovix-solutions.com>
2023-10-07 18:15:13 +0630

    Implement Dynamic Result

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('Hi')
+var scoreContainer = document.getElementById('score-container');
+let total = 0;
+
+fetch('data.json')
+    .then((Response) => Response.json())
+    .then((json) => {
+        for (let i = 0; i < json.length; i++) {
+            data = json[i]
+            scoreItem = document.createElement('div');
+            scoreItem.innerHTML = `
+            <div id="" class="score-item ${data['color']} d-flex justify-content-between align-items-center fw-bold">
+                <div class="d-flex">
+                    <img src="" alt="" srcset="${data['icon']}">
+                    <span class="px-2">${data['category']}</span>
+                </div>
+                <div>
+                    <span class="res-mark">${data['score']}</span>
+                    <span class="text-secondary"> / 100</span>
+                </div>
+            </div>
+            `;
+            scoreContainer.appendChild(scoreItem)
+            total += data['score']
+        }
+
+        avg_total = Math.floor(total / json.length)
+        document.getElementById('score').innerText = avg_total
+    })
